c6bffed840fbcb8d51ea228e56d6113505f3e78b
Rob Meijer <pibara@gmail.com>
2021-04-12 13:45:22 +0200

    partial code cleanup, more needed

--- spqsigs/__init__.py
+++ spqsigs/__init__.py
@@ -10,8 +10,8 @@ import os
 import struct
 import base64
 from hashlib import blake2b
-from bitstring import BitArray
 import concurrent.futures
+from bitstring import BitArray
 
 def _deep_bytes_to_b64(obj):
     if isinstance(obj, dict):
@@ -23,8 +23,8 @@ def _deep_bytes_to_b64(obj):
         return obj
     if isinstance(obj, list):
         rval = list()
-        for o2 in obj:
-            rval.append(_deep_bytes_to_b64(o2))
+        for obj2 in obj:
+            rval.append(_deep_bytes_to_b64(obj2))
         return rval
     if isinstance(obj, bytes):
         return base64.b64encode(obj).decode()
@@ -40,8 +40,8 @@ def _deep_b64_to_bytes(obj):
         return obj
     if isinstance(obj, list):
         rval = list()
-        for o2 in obj:
-            rval.append(_deep_b64_to_bytes(o2))
+        for obj2 in obj:
+            rval.append(_deep_b64_to_bytes(obj2))
         return rval
     if isinstance(obj, str):
         return base64.b64decode(obj.encode())
@@ -113,8 +113,71 @@ def _onekey(onekey, hashfunction, wotsbits, salt, ndx):
         onekey_pub.append(public_wotspair)
     return [onekey_pub, ndx]
 
+def _key_from_seed(state, key_count, subkeys_per_key, hashfunction):
+    rval = list()
+    for index in range(0, key_count):
+        onekey = list()
+        for index2 in range(0, subkeys_per_key):
+            wotspair = list()
+            for direction in [b"L", b"R"]:
+                unsalted = hashfunction(str(index).encode() +
+                                        direction +
+                                        str(index2).encode(),
+                                        key=state["seed"])
+                wotspair.append(hashfunction(unsalted,
+                                             key=state["salt"]))
+            onekey.append(wotspair)
+        rval.append(onekey)
+    return rval
+
+def _digest_to_numlist(digest, state):
+    digest_bits = list(BitArray(bytes=digest).bin)
+    bitlists = list()
+    while len(digest_bits) > state["wotsbits"]:
+        bitlists.append(digest_bits[:state["wotsbits"]])
+        digest_bits = digest_bits[state["wotsbits"]:]
+    bitlists.append(digest_bits)
+    numlist = list()
+    for bits in bitlists:
+        num = 0
+        for bit in bits:
+            num *= 2
+            if bit == "1":
+                num += 1
+        numlist.append(num)
+    return numlist
+
+def _create_signature_body(numlist, state, private_key, hashfunction):
+    signature_body=list()
+    # pylint: disable=consider-using-enumerate
+    for index in range(0,len(numlist)):
+        count1 = numlist[index]
+        count2 = int(math.pow(2, state["wotsbits"])) - count1 - 1
+        val1 = private_key[index][0]
+        val2 = private_key[index][1]
+        for _ in range (0, count1):
+            val1 = hashfunction(val1, state["salt"])
+        for _ in range (0, count2):
+            val2 = hashfunction(val2, state["salt"])
+        signature_body.append([val1,val2])
+    return signature_body
+
+def _create_signature_merkletree_header(state, merkletree):
+    signature_header_mt = list()
+    bitindex = BitArray("{0:#0{1}x}".format(state["next"],
+                                            34)).bin[-state["merkledepth"]:]
+    for index in range(0, state["merkledepth"]):
+        prefix = bitindex[0:index]
+        thisbit = bitindex[index]
+        if thisbit == "1":
+            signature_header_mt.append(merkletree[prefix + "0"])
+        else:
+            signature_header_mt.append(merkletree[prefix + "1"])
+    return signature_header_mt
+
 class SigningKey:
     """Class representing a hash based signing key"""
+    # pylint: disable=too-many-arguments
     def __init__(self, wotsbits=12, merkledepth=10, hashlen=24, restore=None, multiproc=8):
         """Constructor should either be called in one of three ways:
         * Without any arguments. A new key is generated using the default settings.
@@ -141,38 +204,32 @@ class SigningKey:
         self.subkeys_per_key = math.ceil(self.hashfunction.bitcount/self.state["wotsbits"])
         self.key_count = int(math.pow(2,self.state["merkledepth"]))
         # (re)-generate secret key from seed.
-        self.private_key = list()
-        for index in range(0, self.key_count):
-            onekey = list()
-            for index2 in range(0,self.subkeys_per_key):
-                wotspair = list()
-                for direction in [b"L", b"R"]:
-                    unsalted = self.hashfunction(str(index).encode() +
-                                                 direction +
-                                                 str(index2).encode(),
-                                                 key=self.state["seed"])
-                    wotspair.append(self.hashfunction(unsalted,
-                                                      key=self.state["salt"]))
-                onekey.append(wotspair)
-            self.private_key.append(onekey)
+        self.private_key = _key_from_seed(self.state,
+                                          self.key_count,
+                                          self.subkeys_per_key,
+                                          self.hashfunction)
         # generate the big pubkey from private key (if not restorable)
         if self.state["pubkey"] is None:
             if multiproc > 1:
                 r01 = len(self.private_key)
                 self.state["pubkey"] = [None] * r01
                 with concurrent.futures.ProcessPoolExecutor(max_workers = multiproc) as executor:
-                    future_to_res = {executor.submit(_onekey, self.private_key[ndx], self.hashfunction, self.state["wotsbits"], self.state["salt"], ndx): ndx for ndx in range(0,r01)}
-                    for future in concurrent.futures.as_completed(future_to_res):
-                        res = future_to_res[future]
-                        data = future.result()
+                    future_to_res = {executor.submit(_onekey,
+                                                     self.private_key[ndx],
+                                                     self.hashfunction,
+                                                     self.state["wotsbits"],
+                                                     self.state["salt"],
+                                                     ndx): ndx for ndx in range(0,r01)}
+                    for onekey in concurrent.futures.as_completed(future_to_res):
+                        data = onekey.result()
                         self.state["pubkey"][data[1]] = data[0]
             else:
                 self.state["pubkey"] = list()
                 for onekey in self.private_key:
-                    self.state["pubkey"].append(_onekey(onekey, 
+                    self.state["pubkey"].append(_onekey(onekey,
                                                         self.hashfunction,
                                                         self.state["wotsbits"],
-                                                        self.state["salt"]))
+                                                        self.state["salt"],0)[0])
         # reduce the public key size
         medium_public_key = list()
         for big_pubkey in self.state["pubkey"]:
@@ -190,44 +247,12 @@ class SigningKey:
         # Get the current private key
         private_key = self.private_key[self.state["next"]]
         # Convert the digest to a list of integers for signing.
-        digest_bits = list(BitArray(bytes=digest).bin)
-        bitlists = list()
-        while len(digest_bits) > self.state["wotsbits"]:
-            bitlists.append(digest_bits[:self.state["wotsbits"]])
-            digest_bits = digest_bits[self.state["wotsbits"]:]
-        bitlists.append(digest_bits)
-        numlist = list()
-        for bits in bitlists:
-            num = 0
-            for bit in bits:
-                num *= 2
-                if bit == "1":
-                    num += 1
-            numlist.append(num)
+        numlist = _digest_to_numlist(digest, self.state)
         # Create the signature body
-        signature_body=list()
-        # pylint: disable=consider-using-enumerate
-        for index in range(0,len(numlist)):
-            count1 = numlist[index]
-            count2 = int(math.pow(2,self.state["wotsbits"])) - count1 - 1
-            val1 = private_key[index][0]
-            val2 = private_key[index][1]
-            for _ in range (0, count1):
-                val1 = self.hashfunction(val1, self.state["salt"])
-            for _ in range (0, count2):
-                val2 = self.hashfunction(val2, self.state["salt"])
-            signature_body.append([val1,val2])
+        signature_body = _create_signature_body(numlist, self.state, private_key,
+                                                self.hashfunction)
         # Create the merkletree signature header
-        signature_header_mt = list()
-        bitindex = BitArray("{0:#0{1}x}".format(self.state["next"],
-                                                34)).bin[-self.state["merkledepth"]:]
-        for index in range(0,self.state["merkledepth"]):
-            prefix = bitindex[0:index]
-            thisbit = bitindex[index]
-            if thisbit == "1":
-                signature_header_mt.append(self.merkletree[prefix + "0"])
-            else:
-                signature_header_mt.append(self.merkletree[prefix + "1"])
+        signature_header_mt = _create_signature_merkletree_header(self.state, self.merkletree)
         # encode the key index
         bindex = struct.pack(">H",self.state["next"])
         # Compose the signature from:
@@ -251,6 +276,7 @@ class SigningKey:
         msg_digest = self.hashfunction(message, self.state["salt"])
         return self.sign_digest(msg_digest)
     def get_state(self):
+        """Get serializable signing key state"""
         return _deep_bytes_to_b64(self.state)
 
 
